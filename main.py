e29ee26df36b8402d859d58b7e6d676720676591
Wordhater <ryanlocke058@gmail.com>
2024-08-17 06:40:40 +1000

    added loop function and soundsnipe command

--- main.py
+++ main.py
@@ -109,8 +109,8 @@ async def stop(ctx):
     else:
         await ctx.send("The bot is not connected to a voice channel.")
 
-@bot.command(name='play', help='Plays sound.', description="Usage: .play [soundname] [channelname (if needed)]")
-async def play(ctx, file="", custom_channel=""):
+@bot.command(name='play', help='Plays sound.', description="Usage: .play [soundname/url] [other arguments(loop)]")
+async def play(ctx, file="", args=""):
     try:
         if not ctx.message.author.voice:
             await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
@@ -130,13 +130,25 @@ async def play(ctx, file="", custom_channel=""):
             if yt_dlp.YoutubeDL(yt_dlp_options).download(file) != 0:
                 await ctx.send("failed to download youtube video")
             else:
-                voice_channel.play(discord.FFmpegPCMAudio(executable=vars['FFMPEG_PATH'], source="tmp/yt_audio"))
                 await ctx.send("playing audio from youtube")
-            
+                if not "loop" in args:
+                    voice_channel.play(discord.FFmpegPCMAudio(executable=vars['FFMPEG_PATH'], source="tmp/yt_audio"))
+                else:
+                    while True:
+                        voice_channel.play(discord.FFmpegPCMAudio(executable=vars['FFMPEG_PATH'], source="tmp/yt_audio"))
+                        while voice_channel.is_playing():
+                            await asyncio.sleep(0.5)
+
     else:
         filepath = path.join("resources", str(ctx.author), file)
         if path_exists(filepath):
-            voice_channel.play(discord.FFmpegPCMAudio(executable=vars['FFMPEG_PATH'], source=filepath))
+            if not "loop" in args:
+                voice_channel.play(discord.FFmpegPCMAudio(executable=vars['FFMPEG_PATH'], source=filepath))
+            else:
+                while True:
+                    voice_channel.play(discord.FFmpegPCMAudio(executable=vars['FFMPEG_PATH'], source=filepath))
+                    while voice_channel.is_playing():
+                        await asyncio.sleep(0.5)
         else:
             await ctx.send("sound does not exist")
     while voice_channel.is_playing():
@@ -144,6 +156,49 @@ async def play(ctx, file="", custom_channel=""):
     if voice_channel.is_connected():
         await voice_channel.disconnect()
 
+# it's a seperate command because it would be annoying to implement
+@bot.command(name='soundsnipe', help='Plays sound in vc your not connected to', description="Usage: .soundsnipe [soundname/url] [channel name(put in quotations if it contains spaces)] [other arguments(loop)]")
+async def soundsnipe(ctx, file="", channelinput="", args=""):
+    try:
+        channel = discord.utils.get(ctx.guild.channels, name=channelinput)
+        await channel.connect()
+    except:
+        ctx.send("ailed to join vc, there may be an issue with the name of the channel.\nNote that you must use quotations around the name if it contains any spaces. E.g: 'general voice'")
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+    # change to use regex for better reliability at some point
+    if file.startswith("http"):
+        await ctx.send("attempting to play youtube video")
+        async with ctx.typing():
+            remove("tmp/yt_audio")
+            if yt_dlp.YoutubeDL(yt_dlp_options).download(file) != 0:
+                await ctx.send("failed to download youtube video")
+            else:
+                await ctx.send("playing audio from youtube")
+                if not "loop" in args:
+                    voice_channel.play(discord.FFmpegPCMAudio(executable=vars['FFMPEG_PATH'], source="tmp/yt_audio"))
+                else:
+                    while True:
+                        voice_channel.play(discord.FFmpegPCMAudio(executable=vars['FFMPEG_PATH'], source="tmp/yt_audio"))
+                        while voice_channel.is_playing():
+                            await asyncio.sleep(0.5)
+
+    else:
+        filepath = path.join("resources", str(ctx.author), file)
+        if path_exists(filepath):
+            if not "loop" in args:
+                voice_channel.play(discord.FFmpegPCMAudio(executable=vars['FFMPEG_PATH'], source=filepath))
+            else:
+                while True:
+                    voice_channel.play(discord.FFmpegPCMAudio(executable=vars['FFMPEG_PATH'], source=filepath))
+                    while voice_channel.is_playing():
+                        await asyncio.sleep(0.5)
+        else:
+            await ctx.send("sound does not exist")
+    while voice_channel.is_playing():
+            await asyncio.sleep(1)
+    if voice_channel.is_connected():
+        await voice_channel.disconnect()
 
 @bot.event
 async def on_ready():
